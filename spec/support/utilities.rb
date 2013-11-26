12f818fca334c3a967693afdf6e440f0e72c991c
guillaume <g.desjonqueres@gmail.com>
2013-11-26 01:48:04 +0100

    Add named routes

---
+++ spec/support/utilities.rb
@@ -0,0 +1,6 @@
+# returns full title on a per-page basis
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  return "#{base_title} | #{page_title}" unless page_title.empty?
+  base_title
+end
