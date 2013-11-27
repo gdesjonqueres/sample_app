6ab6d1742b70c23925650da56f5b0d47375e81f3
guillaume <g.desjonqueres@gmail.com>
2013-11-27 19:20:21 +0100

    Improve RSpec

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,6 +1,1 @@
-# returns full title on a per-page basis
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  return "#{base_title} | #{page_title}" unless page_title.empty?
-  base_title
-end
+include ApplicationHelper
